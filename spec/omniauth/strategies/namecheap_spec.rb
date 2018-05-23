dc1272ab9b761e85294b06c6fbeb5dd6e1356121
Rachel Carvalho <rachel.carvalho@gmail.com>
2018-05-23 14:56:59 -0400

    supply an env to the strategy for better specs

--- spec/omniauth/strategies/namecheap_spec.rb
+++ spec/omniauth/strategies/namecheap_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 RSpec.describe OmniAuth::Strategies::Namecheap do
   let(:response) { double(:response, body: File.read('spec/fixtures/user_info.json')) }
+  let(:env)      { { "rack.session" => {} } }
+  let(:instance) { described_class.new(nil) }
 
-  subject { described_class.new(nil) }
+  subject { instance }
 
   before do
-    allow(subject).to receive(:access_token) { double(:access_token, get: response, token: 'tolkien') }
+    instance.instance_variable_set :@env, env
+    allow(instance).to receive(:access_token) { double(:access_token, get: response, token: 'tolkien') }
   end
 
   it { is_expected.to be_an OmniAuth::Strategies::OAuth2 }
@@ -39,15 +42,17 @@ RSpec.describe OmniAuth::Strategies::Namecheap do
   describe '#build_access_token' do
     let(:token) { double(:token) }
 
+    subject { instance.build_access_token }
+
     before do
-      allow(subject.request).to receive(:params) { { 'code' => 'lalala' } }
-      allow(subject).to receive(:query_string) { '?this=query' }
-      allow(subject).to receive(:callback_url) { 'https://reddit.com/r/hmmm?this=query' }
+      allow(instance.request).to receive(:params) { { 'code' => 'lalala' } }
+      allow(instance).to receive(:query_string) { '?this=query' }
+      allow(instance).to receive(:callback_url) { 'https://reddit.com/r/hmmm?this=query' }
     end
 
     it 'gets token with the code and redirect uri' do
-      expect(subject).to receive_message_chain(:client, :auth_code, :get_token).with('lalala', { redirect_uri: 'https://reddit.com/r/hmmm' }, {}) { token }
-      expect(subject.build_access_token).to eq token
+      expect(instance).to receive_message_chain(:client, :auth_code, :get_token).with('lalala', { redirect_uri: 'https://reddit.com/r/hmmm' }, {}) { token }
+      expect(subject).to eq token
     end
   end
 end
